ad0979e978ffd90bae6cdd2e797f760ca82df809
GKapppa <dan.pintoc@duocuc.cl>
2026-05-06 19:15:38 -0400

    Agrega modelos Region y comuna

--- src/main/java/com/loveu/loveu/model/Comuna.java
+++ src/main/java/com/loveu/loveu/model/Comuna.java
@@ -1,5 +1,39 @@
 package com.loveu.loveu.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="comunas")
 public class Comuna {
 
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message="El nombre de la comuna no puede estar vacio!")
+    @Column(name="nombre_comuna", length=50)
+    @Size(max=50, message="El nombre de la comuna no puede pasar los 50 caracteres!")
+    private String nombreComuna;
+
+    @ManyToOne
+    @JoinColumn(name="region_id", referencedColumnName="id") //Hacemos relacion Many to one ya que esta clase guarda el ID de Region 
+    private Region region;
 }
