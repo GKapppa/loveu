c2ce7fcb69877121da25f6b7c19bc1ac10831fe5
GKapppa <dan.pintoc@duocuc.cl>
2026-06-09 23:59:14 -0400

    v2: eliminar loveu-main (movido a loveu-v1)

--- loveu-main/src/main/java/com/loveu/loveu/model/EstadoReporte.java
+++
@@ -1,4 +0,0 @@
-package com.loveu.loveu.model;
-
-// Creamos el ENUM de reporte para así poder tener solamente 3 estados en nuestro reporte y no tener que preocuparnos por errores de tipeo o estados no validos.
-public enum EstadoReporte {EN_REVISION, RESUELTO, RECHAZADO}
